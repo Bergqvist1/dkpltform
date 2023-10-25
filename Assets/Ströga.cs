83639ab57a3d852259ff41ceeca5b0e2b792863f
Bergqvist1 <isel06002@utb.vaxjo.se>
2023-10-25 14:15:32 +0200

    11111111

---
+++ Assets/Ströga.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ströga : MonoBehaviour
+{
+
+    public float speed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if(Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0,0, speed * Time.deltaTime);
+        }
+
+        if(Input.GetKey(KeyCode.S)){
+            transform.position += new Vector3(0,0, -speed * Time.deltaTime);
+        }
+
+        if(Input.GetKey(KeyCode.D)){
+            transform.position += new Vector3(speed * Time.deltaTime,0,0);
+        }
+
+        if(Input.GetKey(KeyCode.A)){
+            transform.position += new Vector3(-speed * Time.deltaTime,0,0);
+        }
+    }
+}
